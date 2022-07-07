c205eb2ada054b498be4263f894b948dd40aa3ae
MattGueler <mateus.gueler@gmail.com>
2022-07-07 01:42:31 -0300

    Cria token com JWT

--- src/Controllers/LoginController.js
+++ src/Controllers/LoginController.js
@@ -6,42 +6,43 @@ import { db } from '../dbMongo/Mongo.js'
 
 export async function LoginUsuario(req, res) {
 
-    const body = req.body;
+    const { email, senha } = req.body;
 
     const userSchema = joi.object({
         email: joi.string().email().required(),
         senha: joi.string().required()
     });
 
-    // try {
-    //     const validation = userSchema.validate({ email, senha }, { abortEarly: true });
+    try {
+        const validation = userSchema.validate({ email, senha }, { abortEarly: true });
 
-    //     if (validation.error) {
-    //         console.log(validation.error.details)
-    //         res.sendStatus(422)
-    //         return
-    //     }
+        if (validation.error) {
+            console.log(validation.error.details)
+            res.sendStatus(422)
+            return
+        }
 
-    //     const existe = await db.collection("users").findOne({
-    //         email
-    //     })
+        // const existe = await db.collection("users").findOne({
+        //     email
+        // })
 
-    //     if (!existe) {
-    //         res.status(401).send('Dados inválidos')
-    //     }
+        // if (!existe) {
+        //     res.status(401).send('Dados inválidos')
+        // }
 
-    //     const autorizado = bcrypt.compareSync(senha, existe.senha)
+        // const autorizado = bcrypt.compareSync(senha, existe.senha)
 
-    //     if (!autorizado) {
-    //         return res.status(401).send('Dados inválidos')
-    //     }
+        // if (!autorizado) {
+        //     return res.status(401).send('Dados inválidos')
+        // }
 
+        const dados = {email,senha};
+        const chaveSecreta = process.env.JWT_SECRET;
+        const token = jwt.sign(dados, chaveSecreta);
 
-
-    //     res.status(200).send('OK')
-    // }
-    // catch {
-    //     return res.sendStatus(500)
-    // }
-    res.send(body)
+        return res.status(200).send(token)
+    }
+    catch {
+        return res.sendStatus(500)
+    }
 }
