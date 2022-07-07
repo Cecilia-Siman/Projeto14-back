31cd60951cc0c9ae5e5c29e04c83fc287e858205
MattGueler <mateus.gueler@gmail.com>
2022-07-07 20:11:04 -0300

    Verifica usuarios online

--- src/Routes/LoginRouter.js
+++ src/Routes/LoginRouter.js
@@ -1,8 +1,11 @@
 import { LoginUsuario } from '../Controllers/LoginController.js'
+import { ConfereOnline } from '../Controllers/LoginController.js'
 import { Router } from 'express'
 
 const server = Router()
 
 server.post('/login', LoginUsuario)
+
+server.get('/login', ConfereOnline)
 
 export default server;
