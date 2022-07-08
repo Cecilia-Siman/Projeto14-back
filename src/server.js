1cb9c4fe920e611e400b23dac8c48440c520cbca
MattGueler <mateus.gueler@gmail.com>
2022-07-07 22:22:49 -0300

    Cria requisições do carrinho

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import LoginRouter from './Routes/LoginRouter.js'
+import CarrinhoRouter from './Routes/CarrinhoRouter.js'
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ server.use(express.json());
 server.use(cors());
 
 server.use(LoginRouter)
+
+server.use(CarrinhoRouter)
 
 server.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
