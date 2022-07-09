05b2b684c70b2596244c16d1fcaf53a8a0ad1d6f
MattGueler <mateus.gueler@gmail.com>
2022-07-09 16:45:03 -0300

    Pega os dados pelo token jwt

--- src/Controllers/CarrinhoController.js
+++ src/Controllers/CarrinhoController.js
@@ -1,8 +1,23 @@
 import { db } from '../dbMongo/Mongo.js'
+import jwt from 'jsonwebtoken';
 
 export async function MostraCarrinho(req, res) {
 
-    const token = req.headers
+    const { authorization } = req.headers
+
+    const token = authorization?.replace('Bearer ', '')
+    console.log(token)
+
+    const chaveSecreta = process.env.JWT_SECRET;
+
+    try {
+        const dados = jwt.verify(token, chaveSecreta);
+        console.log(dados)
+
+    } catch {
+        alert('seu token foi adulterado!')
+        res.send(401)
+    }
     const produtosCarrinho = await db.collection("carrinho").find().toArray()
     res.send(produtosCarrinho)
 }
