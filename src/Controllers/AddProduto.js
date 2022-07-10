a1a9c4fab5e9a0f721aa97075cf161d21df7b6bd
MattGueler <mateus.gueler@gmail.com>
2022-07-10 19:29:49 -0300

    Adiciona produtos no banco de dados

--- src/Controllers/AddProduto.js
+++ src/Controllers/AddProduto.js
@@ -45,33 +45,27 @@ export async function AddProduto(req, res) {
 
     const valid = userSchema.validate(req.body);
 
-    if (!valid.error) {
+    // if (!valid.error) {
 
-        // const novoProduto = {
-        //     imagem,
-        //     nome,
-        //     tipo,
-        //     preco,
-        //     descricao
-        // };
+    //     const novaGalaxia = {
+    //         galaxia,
+    //         estoque: []
+    //     }
 
-        const novaGalaxia = {
-            galaxia,
-            estoque: []
-        }
+    //     const galaxias = await db.collection("produtos").findOne({ galaxia: galaxia });
 
-        const galaxias = await db.collection("produtos").findOne({ galaxia: galaxia });
+    //     if (!galaxias) {
+    //         await db.collection("produtos").insertOne(novaGalaxia);
+    //         res.status(201).send('Adicionado nova galaxia');
+    //     }
+    //     else {
+    //         await db.collection("produtos").insertOne(novoProduto);
+    //         res.status(422).send('Essa galaxia ja existe')
+    //     }
+    // }
+    // else {
+    //     res.status(422).send(valid.error.details);
+    // }
 
-        if (!galaxias) {
-            await db.collection("produtos").insertOne(novaGalaxia);
-            res.status(201).send('Adicionado nova galaxia');
-        }
-        else {
-            await db.collection("produtos").insertOne(novoProduto);
-            res.status(422).send('Essa galaxia ja existe')
-        }
-    }
-    else {
-        res.status(422).send(valid.error.details);
-    }
+    res.send(200)
 }
