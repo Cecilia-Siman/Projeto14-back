8c63deb728a433ddf72862abf88e8797f8c48009
MattGueler <mateus.gueler@gmail.com>
2022-07-10 00:41:58 -0300

    Adiciona produto com o id do usuário no carrinho

--- src/Controllers/CarrinhoController.js
+++ src/Controllers/CarrinhoController.js
@@ -11,10 +11,9 @@ export async function MostraCarrinho(req, res) {
 
     try {
         const dados = jwt.verify(token, chaveSecreta);
-        const meUsuraio = await db.collection("users").find({ email: dados.email }).toArray()
-        const idUser = objectId(meUsuraio._id)
-        const produtosCarrinho = await db.collection("carrinho").findOne({ idUser: idUser })
-        res.send(idUser)
+        const meUsuraio = await db.collection("users").findOne({ email: dados.email })
+        const produtosCarrinho = await db.collection("carrinho").findOne({ idUser: objectId(meUsuraio._id) })
+        res.send(produtosCarrinho)
 
     } catch {
         // alert('seu token foi adulterado ou passou da validade!')
@@ -41,7 +40,8 @@ export async function AdicionaCarrinho(req, res) {
         const idUser = objectId(meUsuraio._id)
         const produtoAdicionado = { ...produto, idUser: idUser }
         await db.collection("carrinho").insertOne(produtoAdicionado)
-        res.send('OK')
+        const produtosCarrinho = await db.collection("carrinho").find().toArray()
+        res.send(produtosCarrinho)
     }
     catch {
         res.status(501).send('Não foi possível verificar seu carrinho!')
