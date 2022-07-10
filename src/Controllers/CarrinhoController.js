4dd9e69024037d12809c13193a93a15e3be9c702
MattGueler <mateus.gueler@gmail.com>
2022-07-10 00:30:10 -0300

    Adiciona produto com o id do usuário no carrinho

--- src/Controllers/CarrinhoController.js
+++ src/Controllers/CarrinhoController.js
@@ -1,4 +1,4 @@
-import { db } from '../dbMongo/Mongo.js'
+import { db, objectId } from '../dbMongo/Mongo.js'
 import jwt from 'jsonwebtoken';
 
 export async function MostraCarrinho(req, res) {
@@ -6,19 +6,19 @@ export async function MostraCarrinho(req, res) {
     const { authorization } = req.headers
 
     const token = authorization?.replace('Bearer ', '')
-    console.log(token)
 
     const chaveSecreta = process.env.JWT_SECRET;
 
     try {
         const dados = jwt.verify(token, chaveSecreta);
-        const meUsuraio = await db.collection("users").find().toArray()
-        // const produtosCarrinho = await db.collection("carrinho").find({}).toArray()
-        res.send(meUsuraio)
+        const meUsuraio = await db.collection("users").find({ email: dados.email }).toArray()
+        const idUser = objectId(meUsuraio._id)
+        const produtosCarrinho = await db.collection("carrinho").find({ idUser: idUser }).toArray()
+        res.send(produtosCarrinho)
 
     } catch {
         // alert('seu token foi adulterado ou passou da validade!')
-        res.send(401)
+        res.status(401).send('Seu token foi adulterado ou passou da validade!')
     }
 
     // res.send(dados)
@@ -27,16 +27,23 @@ export async function MostraCarrinho(req, res) {
 
 export async function AdicionaCarrinho(req, res) {
 
-    const token = req.headers
+    const { authorization } = req.headers
+
+    const token = authorization?.replace('Bearer ', '')
+
+    const chaveSecreta = process.env.JWT_SECRET;
 
     const produto = req.body
 
     try {
-        const produtosCarrinho = await db.collection("carrinho").insertOne(produto)
+        const dados = jwt.verify(token, chaveSecreta);
+        const meUsuraio = await db.collection("users").findOne({ email: dados.email })
+        const idUser = objectId(meUsuraio._id)
+        const produtoAdicionado = { ...produto, idUser: idUser }
+        await db.collection("carrinho").insertOne(produtoAdicionado)
         res.send('OK')
     }
     catch {
         res.status(501).send('Não foi possível verificar seu carrinho!')
     }
 }
-
