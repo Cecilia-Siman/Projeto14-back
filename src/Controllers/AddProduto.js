cafe9de125ff14c821091e82a41037dc38b57940
MattGueler <mateus.gueler@gmail.com>
2022-07-10 20:40:57 -0300

    Adiciona novo produto a collection de produtos

--- src/Controllers/AddProduto.js
+++ src/Controllers/AddProduto.js
@@ -67,7 +67,7 @@ export async function AddProduto(req, res) {
         //     // }
         // const estoque = galaxias.estoque
         //     // const novoEstoque = estoque.push(novoProduto)
-        return res.send(galaxia)
+        return res.send(galaxias)
     }
     // // else {
     // //     res.status(422).send(valid.error.details);
