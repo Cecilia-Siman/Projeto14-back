5f690da905772478e1b68c9ba1b0c66af1d3b90f
MattGueler <mateus.gueler@gmail.com>
2022-07-10 19:23:53 -0300

    Adiciona produtos no banco de dados

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import LoginRouter from './Routes/LoginRouter.js'
 import CarrinhoRouter from './Routes/CarrinhoRouter.js'
 import CadastroRouter from './Routes/CadastroRouter.js'
+import AddProdutoRouter from './Routes/AddProdutoRouter.js'
 import validateUser from './Middlewares/ValidateUser.js'
 
 dotenv.config();
@@ -16,7 +17,7 @@ server.use(LoginRouter);
 server.use(CadastroRouter);
 
 server.use(validateUser, CarrinhoRouter)
-server.use(CadastraProduto)
+server.use(AddProdutoRouter)
 
 server.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
