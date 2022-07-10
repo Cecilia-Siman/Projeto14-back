a9327dfe9364d1b5c8985500149a73394ea0cb76
MattGueler <mateus.gueler@gmail.com>
2022-07-10 11:57:04 -0300

    Usa validação de usuário antes de chamar a função para mostrar/adicionar carrinho

--- src/Controllers/CarrinhoController.js
+++ src/Controllers/CarrinhoController.js
@@ -7,16 +7,15 @@ export async function MostraCarrinho(req, res) {
 
     const body = res.locals.body
 
-    // try {
+    try {
 
-    //     const meUsuraio = await db.collection("users").findOne({ email: dados.email })
-    //     const produtosCarrinho = await db.collection("carrinho").find({ idUser: objectId(meUsuraio._id) }).toArray()
-    //     res.send(produtosCarrinho)
+        const meUsuraio = await db.collection("users").findOne({ email: dados.email })
+        const produtosCarrinho = await db.collection("carrinho").find({ idUser: objectId(meUsuraio._id) }).toArray()
+        res.send(produtosCarrinho)
 
-    // } catch {
-    //     res.status(401).send('Usuário não encontrado!')
-    // }
-    res.send(dados)
+    } catch {
+        res.status(401).send('Usuário não encontrado!')
+    }
 }
 
 // Adiciona o produto escolhido no carrinho com o id do meu usuário
@@ -26,16 +25,15 @@ export async function AdicionaCarrinho(req, res) {
 
     const produto = res.locals.body
 
-    // try {
-    //     const meUsuraio = await db.collection("users").findOne({ email: dados.email })
-    //     // const idUser = objectId(meUsuraio._id)
-    //     // const produtoAdicionado = { ...produto, idUser: idUser }
-    //     // await db.collection("carrinho").insertOne(produtoAdicionado)
-    //     // const produtosCarrinho = await db.collection("carrinho").find().toArray()
-    //     res.send(meUsuraio)
-    // }
-    // catch {
-    //     res.status(501).send('Não foi possível verificar seu carrinho!')
-    // }
-    res.send(produto)
+    try {
+        const meUsuraio = await db.collection("users").findOne({ email: dados.email })
+        const idUser = objectId(meUsuraio._id)
+        const produtoAdicionado = { ...produto, idUser: idUser }
+        await db.collection("carrinho").insertOne(produtoAdicionado)
+        const produtosCarrinho = await db.collection("carrinho").find().toArray()
+        res.send(meUsuraio)
+    }
+    catch {
+        res.status(501).send('Não foi possível verificar seu carrinho!')
+    }
 }
