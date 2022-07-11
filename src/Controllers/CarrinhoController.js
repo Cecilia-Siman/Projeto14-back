adf4c44723f67e83eea4139816ef8d6424ea59d7
MattGueler <mateus.gueler@gmail.com>
2022-07-11 14:17:28 -0300

    Deleta apenas um produto

--- src/Controllers/CarrinhoController.js
+++ src/Controllers/CarrinhoController.js
@@ -52,7 +52,7 @@ export async function RemoveCarrinho(req, res) {
         // const produtoAdicionado = { ...produto, idUser: idUser }
         // await db.collection("carrinho").insertOne(produtoAdicionado)
         const produtApagar = await db.collection("carrinho").find(
-            { $and: [{ nome: produto.nome }, { nome: { $exists: true } }] })
+            { $and: [{ nome: produto.nome }] })
         res.send(produtApagar)
     }
     catch {
